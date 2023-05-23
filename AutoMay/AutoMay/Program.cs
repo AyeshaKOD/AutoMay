28bbc4def1d378938e6bb43d66eb6c3227f755e1
AyeshaKOD <nilashinie@gmail.com>
2023-05-23 19:40:48 +1200

    added code

--- AutoMay/AutoMay/Program.cs
+++ AutoMay/AutoMay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 internal class Program
 {
@@ -15,11 +16,11 @@ internal class Program
 
         //Enter username
         IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
-        usernameTextbox.SendKeys("ahasnetha");
+        usernameTextbox.SendKeys("hari");
 
         //Enter password
         IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
-        passwordTextbox.SendKeys("an1234");
+        passwordTextbox.SendKeys("123123");
 
         //Click Login button
         IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
@@ -29,7 +30,7 @@ internal class Program
         //check whether a user has been successfully logged in or not 
         IWebElement displayUserTextbox = driver.FindElement(By.XPath("/html/body/div[3]/div/div/form/ul/li/a"));
 
-        if (displayUserTextbox.Text == "Hello ahasnetha!")
+        if (displayUserTextbox.Text == "Hello hari!")
         {
             Console.WriteLine("User has successfully logged in");
 
@@ -56,7 +57,7 @@ internal class Program
         IWebElement typeCodeDropdown = driver.FindElement(By.XPath("/html/body/div[4]/form/div/div[1]/div/span[1]/span/span[1]"));
         typeCodeDropdown.Click();
 
-        IWebElement timeOption = driver.FindElement(By.XPath("/html/body/div[5]/div/ul/li[2]"));
+        IWebElement timeOption = driver.FindElement(By.XPath("/html/body/div[5]/div/ul/li[2]")); 
         timeOption.Click();
 
         IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
@@ -71,5 +72,79 @@ internal class Program
         //click save button to submit those details and create a record 
         IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
         saveButton.Click();
+        Thread.Sleep(1000);
+
+        //Go to last page to find our record 
+        IWebElement lastButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+        lastButton.Click();
+        Thread.Sleep(2000);
+
+        //find the last record on list page 
+        IWebElement lastCodeText = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+        
+        if (lastCodeText.Text == "AutoMay")
+        {
+            Console.WriteLine("New record created successfully");
+        }
+        else
+        {
+            Console.WriteLine("New record was not created");
+        }
+
+
+        // edit record AutoMay
+        
+        //we select a record to edit
+        if (lastCodeText.Text == "AutoMay")
+        {
+            IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+            editButton.Click();
+
+            IWebElement codeEditText = driver.FindElement(By.XPath("//*[@id=\"Code\"]"));
+            codeEditText.Clear();
+            codeEditText.SendKeys("ExMay");
+
+            IWebElement saveEditButton = driver.FindElement(By.Id("SaveButton"));
+            saveEditButton.Click();
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Record code edited successfully");
+
+        }
+        else
+        {
+            Console.WriteLine("Record not edited successfully");
+        }
+
+        // delete record 
+        //Select a record from last  page 
+        IWebElement lastdelButton = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[4]/a[4]/span"));
+        lastdelButton.Click();
+
+        //IWebElement toDelText = driver.FindElement(By.Id("/html/body/div[4]/div/div/div[3]/table/tbody/tr[last()]/td[1]"));
+        if (lastCodeText.Text == "AutoMay")
+        {
+            IWebElement deleteButton = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table/tbody/tr[2]/td[5]/a[2]"));
+            deleteButton.Click();
+            Thread.Sleep(3000);
+
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept();
+            Thread.Sleep(2000);
+
+            //Check the list again to make sure the record is deleted 
+            if (lastButton.Text == string.Empty)
+            {
+                Console.WriteLine("Item deleted successfully ");
+            }
+            else
+            {
+                Console.WriteLine("Item not deleted successfully");
+            }
+            }
+        }
+
+
+
     }
-}
+
